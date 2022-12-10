f10ed9d58c273a71c4b5ce5266ac18543c374272
Sachit_Sankhe <sachitsankhe@gmail.com>
2022-12-11 01:29:13 +0530

    Intial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
